32e4ba3a21d9826866f0eec1fdfd004135b3b8c0
Silas <silas.ottmann@gmail.com>
2024-06-17 17:38:49 +0200

    Added 'is a number prime?'

---
+++ 6 kyu/src/de/sijuot/codewars/Prime.java
@@ -0,0 +1,24 @@
+package de.sijuot.codewars;
+
+public class Prime {
+
+    /**
+     * Is a number prime?
+     * Prüft, ob die eingegebene Zahl eine Primzahl ist.
+     * @param num Die zu prüfende Zahl.
+     * @return True, falls es eine Primzahl. False, falls es keine Primzahl ist.
+     */
+    public static boolean isPrime(int num) {
+        // Alle Zahlen, die unter oder gleich 1 sind, sind keine Primzahl.
+        if(num <= 1)
+            return false;
+
+        // Schleife von 2 bis zur aufgerundeten Quadratwurzel der Zahl.
+        for(int i = 2; i < (int)Math.sqrt(num)+1; i++) {
+            // Wenn die Zahl ohne Rest durch i teilbar ist, ist sie keine Primzahl.
+            if(num % i == 0) return false;
+        }
+        return true;
+    }
+
+}
