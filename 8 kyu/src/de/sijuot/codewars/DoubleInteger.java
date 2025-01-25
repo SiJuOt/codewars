2a12e170893e651be93b522964cb2c410d35b384
Silas <silas.ottmann@gmail.com>
2025-01-25 15:29:57 +0100

    Added 'You Can't Code Under Pressure #1'

---
+++ 8 kyu/src/de/sijuot/codewars/DoubleInteger.java
@@ -0,0 +1,12 @@
+package de.sijuot.codewars;
+
+public class DoubleInteger {
+
+    /**
+     * Verdoppelt den Integer.
+     */
+    public static int doubleInteger(int i) {
+        // Double the integer and return it!
+        return i*2;
+    }
+}
