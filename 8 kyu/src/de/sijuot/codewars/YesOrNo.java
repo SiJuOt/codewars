8f9981a40ec84abce4e33739447b8b27e6a4f5ca
Silas <silas.ottmann@gmail.com>
2025-01-25 15:23:14 +0100

    Added "Convert boolean values to String 'Yes' or 'No'"

---
+++ 8 kyu/src/de/sijuot/codewars/YesOrNo.java
@@ -0,0 +1,14 @@
+package de.sijuot.codewars;
+
+public class YesOrNo {
+
+    /**
+     * Naja  gibt halt einen Boolean als String aus.
+     * @param b Boolean
+     * @return "Yes", wenn der Boolean true ist und "No", wenn er falsch ist.
+     */
+    public static String boolToWord(boolean b) {
+        return b ? "Yes" : "No";
+    }
+
+}
