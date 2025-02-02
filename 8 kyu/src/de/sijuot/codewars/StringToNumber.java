586176d9d8dfa8002a0065581e99b3716082dbbc
Silas <silas.ottmann@gmail.com>
2025-02-02 19:26:04 +0100

    Added 'Convert a String to a Number!'

---
+++ 8 kyu/src/de/sijuot/codewars/StringToNumber.java
@@ -0,0 +1,14 @@
+package de.sijuot.codewars;
+
+public class StringToNumber {
+
+    /**
+     * Konvertiert einen String in einen Integer
+     * @param str Der String welcher konvertiert werden soll.
+     * @return Der konvertierte String als Integer.
+     */
+    public static int stringToNumber(String str) {
+        return Integer.parseInt(str);
+    }
+
+}
