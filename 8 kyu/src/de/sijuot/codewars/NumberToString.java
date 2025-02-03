ed776764c05e1833f05411636bb1f266f5d426c8
Silas <silas.ottmann@gmail.com>
2025-02-03 22:40:56 +0100

    Added 'Convert a Number to a String!'

---
+++ 8 kyu/src/de/sijuot/codewars/NumberToString.java
@@ -0,0 +1,14 @@
+package de.sijuot.codewars;
+
+public class NumberToString {
+
+    /**
+     * Konvertiert eine Zahl in einen String.
+     * @param num Die Zahl, welche als String dargestellt werden soll.
+     * @return Der String mit Zahl.
+     */
+    public static String numberToString(int num) {
+        return Integer.toString(num);
+    }
+
+}
