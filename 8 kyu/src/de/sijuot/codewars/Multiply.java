43d9175291e6622286f171807edc59ba5394bcb2
Silas <silas.ottmann@gmail.com>
2025-02-03 23:53:12 +0100

    Added 'Multiply'

---
+++ 8 kyu/src/de/sijuot/codewars/Multiply.java
@@ -0,0 +1,15 @@
+package de.sijuot.codewars;
+
+public class Multiply {
+
+    /**
+     * Multipliziert zwei Doubles.
+     * @param a Faktor A
+     * @param b Faktor B
+     * @return Produkt
+     */
+    public static Double multiply(double a, double b) {
+        return a * b;
+    }
+
+}
