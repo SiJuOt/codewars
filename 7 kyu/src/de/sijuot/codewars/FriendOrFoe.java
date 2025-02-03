947a7b74a3001eb842a1037a19f928c91ac19bb1
Silas <silas.ottmann@gmail.com>
2025-02-03 22:44:16 +0100

    Added 'Friend or Foe?'

---
+++ 7 kyu/src/de/sijuot/codewars/FriendOrFoe.java
@@ -0,0 +1,24 @@
+package de.sijuot.codewars;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FriendOrFoe {
+
+    /**
+     * Gibt eine List zurück, welche nur deine Freunde enthält.
+     * Wenn ein Name exakt 4 Buchstaben lang ist, kann man sich sicher sein, dass jener dein Freund ist.
+     * Sonst kannst du dir nicht sicher sein
+     * @param x Eine Liste voller Namen.
+     * @return Eine Liste mit deinen Freunden
+     */
+    public static List<String> friend(List<String> x) {
+        List<String> friends = new ArrayList<String>();
+        for(String name : x) {
+            if(name.length() == 4)
+                friends.add(name);
+        }
+        return friends;
+    }
+
+}
