b737592dad74f5ab4d06996552dbb628d66058fb
Silas <silas.ottmann@gmail.com>
2025-02-03 22:27:50 +0100

    Updated 'Century From Year'

---
+++ 8 kyu/src/de/sijuot/codewars/CenturyFromYear.java
@@ -0,0 +1,14 @@
+package de.sijuot.codewars;
+
+public class CenturyFromYear {
+
+    /**
+     * Returns the Century of the year.
+     * @param year The Year to get the century.
+     * @return the century
+     */
+    public static int century(int year) {
+        return (year + 99) / 100;
+    }
+
+}
