5fd1b96ed5a515ff19fdafbe21462b2847c96c50
Silas <silas.ottmann@gmail.com>
2025-02-10 22:01:15 +0100

    Added 'Convert a String to an Array'

---
+++ 8 kyu/src/de/sijuot/codewars/StringToArray.java
@@ -0,0 +1,14 @@
+package de.sijuot.codewars;
+
+public class StringToArray {
+
+    /**
+     * Konvertiert die Wörter in einem String in ein Array
+     * @param s String
+     * @return Array
+     */
+    public static String[] stringToArray(String s) {
+        return s.split(" ");
+    }
+
+}
